f8f7b4312641a6018e6869d1022d105b06c2c911
paristsai <petertsai5566168@gmail.com>
2016-04-19 08:37:19 +0800

    fix something stupid
    1. remove False element in the result list  after fetching review pages
    2. fix color, author parsers and add product_id into database

--- crawler/5_get_review.py
+++ crawler/5_get_review.py
@@ -10,6 +10,7 @@ from review_parser import Review
 N = 0
 
 LOCAL = "http://localhost:8080/proxy"
+# PROXY_PATTERN = "http://proxy{0}.appspot.com/proxy"
 
 GETNAME = re.compile(r'product-reviews/(.+)/')
 REVIEWURL = "http://www.amazon.com/product-reviews/{0}/ref=cm_cr_dp_see_all_summary?ie=UTF8&showViewpoints=1&sortBy=recent&pageNumber={1}&pageSize=50"
@@ -26,20 +27,21 @@ def proxyGetPage(url):
 	#product id
 	name = GETNAME.search(url).group(1)
 	#avoid blocked
-	timeToSleep = float(random.randint(15, 30)) / 15
+	timeToSleep = float(random.randint(15, 30)) / 30
 	time.sleep(timeToSleep)
 
 	N += 1
 	print "%4d: fetching %s..." % (N, url.strip())
 	server = PROXY[random.randint(0, len(PROXY) - 1)]
 	proxy_url = "".join([LOCAL, "?", urllib.urlencode({"data":url.encode()})])
-	# proxy_url = "".join(server, "?", urllib.urlencode({"data":url.encode()}))
+	# proxy_url = "".join([server, "?", urllib.urlencode({"data":url.encode()})])
 
 	try:
+		# content = "testcontent"
 		content = urllib.urlopen(proxy_url).read()
 		print "size:%s, from:%s" % (len(content), server)
-		if len(content) > 5000:
-			return content
+		if len(content) > 10000:
+			return {"pid":name, "html":content}
 		else:
 			#record link need to reparse
 			with open("review_error_links", "a") as fp:
@@ -49,27 +51,28 @@ def proxyGetPage(url):
 		print e, e.message
 		return False
 
-	return content
 
 def getReview(urls):
-	contents = map(proxyGetPage, urls)
-	for html in contents:
+	results = map(proxyGetPage, urls)
+	# print(results)
+	# avoid error of fetching review page stopping process
+	results = [x for x in results if x != False]
+	for obj in results:
 		# print(html)
 		#parse
-		reviews = Review(html)
-		# print(reviews.date)
-		#insert to database
+		reviews = Review(obj["pid"], obj["html"])
+		#save result into database
 		reviews.save()
-
+		# reviews.rows()
 if __name__ == '__main__':
 	lines = []
 
 	for line in fileinput.input():
 		lines.append(line)
 
-	startPoint = 1
+	startPoint = 700
 	lines = lines[startPoint - 1:]
-	lines = lines[:1]#DEBUG
+	lines = lines[:500]
 	links = map(formUrl, lines)
 	# print(links)
 	result = map(getReview, links)
--- crawler/review_parser.py
+++ crawler/review_parser.py
@@ -2,11 +2,14 @@ import re
 from pyquery import PyQuery
 import sqlite3 as lite
 con = lite.connect('review.db')
-# When you first exute, you need to create table
+# cur = conn.cursor()
+# Create table
 # con.execute('''CREATE TABLE reviews
-#              (id TEXT UNIQUE, author TEXT, rate INTEGER, title TEXT, content TEXT, date TEXT, verified INTEGER, color TEXT, vote INTEGER)''')
+#              (id TEXT, pid TEXT, author TEXT, rate INTEGER, title TEXT, content TEXT, date TEXT, verified INTEGER, color TEXT, vote INTEGER)''')
+# from sys import getsizeof
 
 VERIFIEDPATTERN = re.compile(r'Verified Purchase')
+COLORPATTERN = re.compile(r'Color: ([^ ]+)')
 VOTEPATTERN = re.compile(r'(person)|(people) found this helpful')
 
 month = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
@@ -36,30 +39,42 @@ def numVote(words):
 def convertTime(date):
 	parts = date.split(" ")
 	return "{0}-{1}-{2}".format(parts[3], int(month.index(parts[1])) + 1, parts[2][:-1])
-
+def findColor(words):
+	color = COLORPATTERN.findall(words)
+	if len(color) > 0:
+		return color[0]
+	else:
+		return ""
+def findAuthor(words):
+	return words.replace("By ", "")
 
 class Review():
-	def __init__(self, html):
+	def __init__(self, pid, html):
 		pq = PyQuery(html)
+		# pq = PyQuery(filename="Amazon_review_test.htm") #DEBUG
 		# if using method1, I need to implement:
 		# total_page = pq(".page-button").eq(-1).text()
 		self.id = [i.attr("id") for i in pq(".a-section.review").items()]
-		self.author = [i.text() for i in pq(".reviews-content .author").items("a")]
+		self.pid = pid
+		self.author = map(findAuthor, [i.text() for i in pq(".reviews-content .review-byline").items()])
 		self.rate = map(lambda i: i[0], [i.text() for i in pq(".reviews-content .review-rating").items('span')])
 		self.title = [i.text() for i in pq(".reviews-content .review-title").items("a")]
 		self.content = [i.text() for i in pq(".reviews-content .a-size-base.review-text").items("span")]
 		self.date = [convertTime(i.text()) for i in pq(".reviews-content .review-date").items("span")]
 		self.verified = map(isVerified, [i.text() for i in pq(".reviews-content .a-row.a-spacing-mini.review-data").items("div")])
-		self.color = [i.text().split(" ")[1] for i in pq(".reviews-content .review-data .a-link-normal.a-color-secondary").items("a")]
+		self.color = map(findColor, [i.text() for i in pq(".reviews-content .a-row.a-spacing-mini.review-data").items("div")])
 		self.vote = map(numVote, [i.text() for i in pq(".reviews-content span.cr-vote-buttons").items()])
-
+		# print(self.color)
 	def rows(self):
 		rows = list()
 		#need to determine length of each array? not sure
+		print "%d\n%d\n%d\n%d\n%d\n%d\n%d\n%d\n%d\n" % (len(self.id),len(self.author),len(self.rate),len(self.title),len(self.content),len(self.date),len(self.verified),len(self.color),len(self.vote))
 		for idx in range(0, len(self.id)):
-			rows.append(tuple([self.id[idx], self.author[idx], self.rate[idx], self.title[idx], self.content[idx], self.date[idx], self.verified[idx], self.color[idx], self.vote[idx]]))
+			rows.append(tuple([self.id[idx], self.pid, self.author[idx], self.rate[idx], self.title[idx], self.content[idx], self.date[idx], self.verified[idx], self.color[idx], self.vote[idx]]))
 		return rows
 	def save(self):
+		#connect sql db
+		# print(self.rows())
 		# Successful, con.commit() is called automatically afterwards
 		# con.rollback() is called after the with block finishes with an exception, the
 		# exception is still raised and must be caught
@@ -67,8 +82,11 @@ class Review():
 			with con:
 				cur = con.cursor()
 				#inserts many records at a time
-				cur.executemany('INSERT INTO reviews VALUES (?,?,?,?,?,?,?,?,?)', self.rows())
+				cur.executemany('INSERT INTO reviews VALUES (?,?,?,?,?,?,?,?,?,?)', self.rows())
 		except lite.Error, e:
 		    if con:
 				con.rollback()
 				print "Error %s:" % e.args[0]
+
+if __name__ == '__main__':
+	r = Review("test123","")
